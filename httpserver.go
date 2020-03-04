8457415e9095ec33e792c64c5e5df2aed132ae53
gfandada <gfandada@gmail.com>
2020-03-04 10:20:06 +0800

    init

---
+++ httpserver.go
@@ -0,0 +1,34 @@
+/*
+Copyright 2019 gfandada@gmail.com.
+*/
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	log.Print("Hello world received a request.")
+	target := os.Getenv("TARGET")
+	if target == "" {
+		target = "gfandada@gmail.com"
+	}
+	fmt.Fprintf(w, "hello world by %s!\n", target)
+}
+
+func main() {
+	log.Print("http server started.")
+
+	http.HandleFunc("/", handler)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+}
